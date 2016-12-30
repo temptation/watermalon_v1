765f5209f3e0a3e1f36752d20421445e76799a96
temptation <124982345@qq.com>
2016-12-30 13:54:28 +0800

    -

---
+++ db/dbconfig.js
@@ -0,0 +1,21 @@
+// MySQL数据库连接配置
+module.exports = {
+    mysql: {
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'sa',
+        database: 'test',
+        port: 3306
+    }
+};
+
+/**
+ 数据库建库建表语句
+ CREATE TABLE customers
+ (
+ userid INT AUTO_INCREMENT PRIMARY KEY,
+ username VARCHAR(10) NOT NULL,
+ email VARCHAR(20) NOT NULL,
+ mobile VARCHAR(11) NOT NULL
+ );
+ */
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+//【设置路由使用的模块】
+// 客户信息列表用模块
+var findAllCustomers = require('../routes/findAllCustomers');
+// 客户新增用模块
+var addCustomerView = require('../routes/addCustomerView');
+var addCustomerHandler = require('../routes/addCustomerHandler');
+// 客户修改用模块
+var editCustomerView = require('../routes/editCustomerView');
+var editCustomerHandler = require('../routes/editCustomerHandler');
+// 客户删除用模块
+var deleteCustomerHandler = require('../routes/deleteCustomerHandler');
+// 客户查询用模块
+var searchCustomerHandler = require('../routes/searchCustomerHandler');
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
 });
+
+//【设置路由】
+// 路由：获取客户信息后，跳转至客户信息列表
+router.get('/customer', findAllCustomers);
+// 路由：跳转至客户新增页面
+router.get('/customer/add', addCustomerView);
+// 路由：执行客户新增后，跳转至客户信息列表
+router.post('/customer/add', addCustomerHandler);
+// 路由：跳转至客户修改页面
+router.get('/customer/edit/:userid', editCustomerView);
+// 路由：执行客户修改后，跳转至客户信息列表
+router.post('/customer/edit/:userid', editCustomerHandler);
+// 路由：执行客户删除后，跳转至客户信息列表
+router.get('/customer/delete/:userid', deleteCustomerHandler);
+// 路由：获取查询信息后，跳转至客户信息列表
+router.post('/customer/search', searchCustomerHandler);
 
 module.exports = router;
